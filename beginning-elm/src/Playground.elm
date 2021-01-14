d9589970de7bcd743ceee3ef1cb9fac7796f510e
Derek Wan <d.wan@icloud.com>
2021-01-14 15:07:33 +0800

    Extend functin

---
+++ beginning-elm/src/Playground.elm
@@ -0,0 +1,56 @@
+module Playground exposing (main)
+
+import Html
+
+
+escapeEarth myVelocity mySpeed fuelStatus =
+    let
+        escapeVelocityInKmPerSec =
+            11.186
+
+        orbitalSpeedInKmPerSec =
+            7.67
+
+        whereToLand fuel =
+            if fuel == "low" then
+                "Land on droneship"
+
+            else
+                "Land on launchpad"
+    in
+    if myVelocity > escapeVelocityInKmPerSec then
+        "Godspeed"
+
+    else if mySpeed == orbitalSpeedInKmPerSec then
+        "Stay in orbit"
+
+    else
+        whereToLand fuelStatus
+
+
+computeSpeed distance time =
+    distance / time
+
+
+computeTime startTime endTime =
+    endTime - startTime
+
+
+add a b =
+    a + b
+
+
+multiply c d =
+    c * d
+
+
+divide e f =
+    e / f
+
+
+main =
+    divide 30 10
+        |> multiply 10
+        |> add 5
+        |> String.fromFloat
+        |> Html.text
