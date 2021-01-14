4f60c9b3c76585a63bf59cf987b62d98236e5fe0
Derek Wan <d.wan@icloud.com>
2021-01-14 12:22:38 +0800

    Add elm-format to local hooks

--- src/Main.elm
+++ src/Main.elm
@@ -1,10 +1,8 @@
+module Main exposing (Model, Msg(..), init, main, update, view)
+
 import Browser
 import Html exposing (Html, button, div, text)
 import Html.Events exposing (onClick)
-
-
-
-
 
 
 
@@ -12,39 +10,49 @@ import Html.Events exposing (onClick)
 
 
 main =
-  Browser.sandbox { init = init, update = update, view = view }
+    Browser.sandbox { init = init, update = update, view = view }
 
 
 
 -- MODEL
 
-type alias Model = Int
+
+type alias Model =
+    Int
+
 
 init : Model
 init =
-  0
+    0
+
 
 
 -- UPDATE
 
-type Msg = Increment | Decrement
+
+type Msg
+    = Increment
+    | Decrement
+
 
 update : Msg -> Model -> Model
 update msg model =
-  case msg of
-    Increment ->
-      model + 1
+    case msg of
+        Increment ->
+            model + 1
 
-    Decrement ->
-      model - 1
+        Decrement ->
+            model - 1
+
 
 
 -- VIEW
 
+
 view : Model -> Html Msg
 view model =
-  div []
-    [ button [ onClick Decrement ] [ text "-" ]
-    , div [] [ text (String.fromInt model) ]
-    , button [ onClick Increment ] [ text "+" ]
-    ]
+    div []
+        [ button [ onClick Decrement ] [ text "-" ]
+        , div [] [ text (String.fromInt model) ]
+        , button [ onClick Increment ] [ text "+" ]
+        ]
