4497b771ffaf5bc21aebfe347761af371489e537
Derek Wan <d.wan@icloud.com>
2021-01-14 20:15:02 +0800

    Add signUp

--- beginning-elm/src/Playground.elm
+++ beginning-elm/src/Playground.elm
@@ -247,6 +247,34 @@ welcomeMessage isLoggedIn =
             "Please log in."
 
 
+signUp : String -> String -> Result String String
+signUp email ageStr =
+    case String.toInt ageStr of
+        Nothing ->
+            Err "Age must be an integer."
+
+        Just age ->
+            let
+                emailPattern =
+                    "\\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}\\b"
+
+                regex =
+                    Maybe.withDefault Regex.never <|
+                        Regex.fromString emailPattern
+
+                isValidEmail =
+                    Regex.contains regex email
+            in
+            if age < 13 then
+                Err "You need to be at least 13 years old to sign up."
+
+            else if isValidEmail then
+                Ok "Your account has been created successfully!"
+
+            else
+                Err "You entered an invalid email."
+
+
 main =
     multiplyByFive 3
         |> Debug.toString
