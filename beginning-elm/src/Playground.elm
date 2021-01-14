2bcfd6e6cad0691a45e9c853292c1147a2126a06
Derek Wan <d.wan@icloud.com>
2021-01-14 15:51:01 +0800

    Add playground

--- beginning-elm/src/Playground.elm
+++ beginning-elm/src/Playground.elm
@@ -1,6 +1,7 @@
 module Playground exposing (main)
 
 import Html
+import Regex
 
 
 escapeEarth myVelocity mySpeed fuelStatus =
@@ -75,6 +76,108 @@ weekday dayInNumber =
             "Unknown day"
 
 
+hashtag dayInNumber =
+    case weekday dayInNumber of
+        "Sunday" ->
+            "#SinDay"
+
+        "Monday" ->
+            "#MondayBlues"
+
+        "Tuesday" ->
+            "#TakeMeBackTuesday"
+
+        "Wednesday" ->
+            "#HumpDay"
+
+        "Thursday" ->
+            "#ThrowbackThursday"
+
+        "Friday" ->
+            "#FlashbackFriday"
+
+        "Saturday" ->
+            "#Caturday"
+
+        _ ->
+            "#Whatever"
+
+
+revelation =
+    """
+    It became very clear to me sitting out there today
+    that every decision I've made in my entire life has
+    been wrong. My life is the complete "opposite" of
+    everything I want it to be. Every instinct I have,
+    in every aspect of life, be it something to wear,
+    something to eat - it's all been wrong.
+    """
+
+
+descending a b =
+    case compare a b of
+        LT ->
+            GT
+
+        GT ->
+            LT
+
+        EQ ->
+            EQ
+
+
+evilometer character1 character2 =
+    case ( character1, character2 ) of
+        ( "Joffrey", "Ramsay" ) ->
+            LT
+
+        ( "Joffrey", "Night King" ) ->
+            LT
+
+        ( "Ramsay", "Joffrey" ) ->
+            GT
+
+        ( "Ramsay", "Night King" ) ->
+            LT
+
+        ( "Night King", "Joffrey" ) ->
+            GT
+
+        ( "Night King", "Ramsay" ) ->
+            GT
+
+        _ ->
+            GT
+
+
+validateEmail email =
+    let
+        emailPattern =
+            "\\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}\\b"
+
+        regex =
+            Maybe.withDefault Regex.never <|
+                Regex.fromString emailPattern
+
+        isValid =
+            Regex.contains regex email
+    in
+    if isValid then
+        ( "Valid email", "green" )
+
+    else
+        ( "Invalid email", "red" )
+
+
+multiplyByFive number =
+    let
+        multiplier =
+            5
+    in
+    number * multiplier
+
+
 main =
-    weekday 5
+    multiplyByFive 3
+        |> Debug.toString
         |> Html.text
---
+++ beginning-elm/src/experiment.js
@@ -0,0 +1,10 @@
+var scoreMultiplier = 2;
+var highestScores = [316, 320, 312, 370, 337, 318, 314];
+
+function doubleScores(scores) {
+  for (var i = 0; i < scores.length; i++) {
+    scores[i] = scores[i] * scoreMultiplier;
+  }
+
+  return scores;
+}
