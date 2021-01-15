ee1376d72b6ddc07fe1a72172abb344650923fd8
Derek Wan <d.wan@icloud.com>
2021-01-16 00:45:07 +0800

    Add tests and move to pre-commit repo (#2)
    * Add elm-tooling
    
    * Init elm-test
    
    * Add the tests
    
    * Add elm/json to top
    
    * Add signUp
    
    * MyList.elm
    
    * Play around with exports & imports
    
    * Add fuzzTests
    
    * More tests
    
    * Save
    
    * Add submodule
    
    * Use pre-commit-hooks submodule
    
    * Add environment
    
    * Bump version: 0.0.1 → 0.0.2
    
    * Remove pre-commit action

--- beginning-elm/src/Playground.elm
+++ beginning-elm/src/Playground.elm
@@ -1,11 +1,15 @@
 module Playground exposing
-    ( doubleScores
+    ( add
+    , doubleScores
+    , guardiansWithShortNames
     , highestScores
     , main
     , scoresLessThan320
+    , signUp
     )
 
-import Html
+import Html exposing (Html, text)
+import MyList exposing (MyList(..), isEmpty)
 import Regex
 
 
@@ -42,6 +46,7 @@ computeTime startTime endTime =
     endTime - startTime
 
 
+add : number -> number -> number
 add a b =
     a + b
 
@@ -202,7 +207,121 @@ isLessThan320 score =
     score < 320
 
 
+guardiansWithShortNames : List String -> Int
+guardiansWithShortNames guardians =
+    guardians
+        |> List.map String.length
+        |> List.filter (\x -> x < 6)
+        |> List.length
+
+
+type Greeting
+    = Howdy
+    | Hola
+    | Namaste String
+    | NumericalHi Int Int
+
+
+sayHello : Greeting -> String
+sayHello greeting =
+    case greeting of
+        Howdy ->
+            "How y'all doin'?"
+
+        Hola ->
+            "Hola amigo!"
+
+        Namaste message ->
+            message
+
+        NumericalHi value1 value2 ->
+            value1 + value2 |> String.fromInt
+
+
+welcomeMessage : Bool -> String
+welcomeMessage isLoggedIn =
+    case isLoggedIn of
+        True ->
+            "Welcome to my awesome site!"
+
+        False ->
+            "Please log in."
+
+
+signUp : String -> String -> Result String String
+signUp email ageStr =
+    case String.toInt ageStr of
+        Nothing ->
+            Err "Age must be an integer."
+
+        Just age ->
+            let
+                emailPattern =
+                    "\\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}\\b"
+
+                regex =
+                    Maybe.withDefault Regex.never <|
+                        Regex.fromString emailPattern
+
+                isValidEmail =
+                    Regex.contains regex email
+            in
+            if age < 13 then
+                Err "You need to be at least 13 years old to sign up."
+
+            else if isValidEmail then
+                Ok "Your account has been created successfully!"
+
+            else
+                Err "You entered an invalid email."
+
+
+type alias Character =
+    { name : String
+    , age : Maybe Int
+    }
+
+
+sansa : Character
+sansa =
+    { name = "Sansa"
+    , age = Just 19
+    }
+
+
+arya : Character
+arya =
+    { name = "Arya"
+    , age = Nothing
+    }
+
+
+getAdultAge : Character -> Maybe Int
+getAdultAge character =
+    case character.age of
+        Nothing ->
+            Nothing
+
+        Just age ->
+            if age >= 18 then
+                Just age
+
+            else
+                Nothing
+
+
+list1 : MyList a
+list1 =
+    Empty
+
+
+list2 : MyList number
+list2 =
+    Node 9 Empty
+
+
+main : Html.Html msg
 main =
-    multiplyByFive 3
+    isEmpty list2
         |> Debug.toString
         |> Html.text
---
+++ beginning-elm/tests/FuzzTests.elm
@@ -0,0 +1,188 @@
+module FuzzTests exposing (addOneTests, addTests, arrayGetTests, flipTests, listLengthTests, listReverseTests, multiplyFloatTests, pizzaLeftTests, stringTests)
+
+import Array
+import Expect exposing (Expectation)
+import Fuzz exposing (..)
+import Http exposing (Expect)
+import Test exposing (..)
+
+
+addOneTests : Test
+addOneTests =
+    describe "test addOne"
+        [ fuzz int "adds 1 to any integer" <|
+            \num ->
+                addOne num |> Expect.equal (num + 1)
+        , test "when 1 is added to 2, the result is 3" <|
+            \_ ->
+                addOne 2 |> Expect.equal 3
+        , fuzzWith { runs = 200 } int "adds 1 to the given integer" <|
+            \x -> addOne x |> Expect.equal (x + 1)
+        , fuzz (intRange -100 100) "adds 1 to the given integer; intRange" <|
+            \x -> addOne x |> Expect.equal (x + 1)
+        , fuzz frequencyFuzzer "adds 1 to the given integer; custom freq" <|
+            \x -> addOne x |> Expect.equal (x + 1)
+        ]
+
+
+addOne : Int -> Int
+addOne x =
+    1 + x
+
+
+add : Int -> Int -> Int
+add x y =
+    x + y
+
+
+addTests : Test
+addTests =
+    describe "add"
+        [ fuzz2 int int "adds two given integers" <|
+            \num1 num2 ->
+                add num1 num2
+                    |> Expect.equal (num1 + num2)
+        ]
+
+
+type alias FuzzOptions =
+    { runs : Int }
+
+
+frequencyFuzzer : Fuzzer Int
+frequencyFuzzer =
+    frequency
+        [ ( 70, constant 7 )
+        , ( 12, intRange 8 9 )
+        , ( 6, constant 6 )
+        , ( 9, intRange 2 4 )
+        , ( 1, constant 5 )
+        , ( 1, constant 1 )
+        , ( 1, constant 10 )
+        ]
+
+
+flip : Bool -> Bool
+flip x =
+    not x
+
+
+flipTests : Test
+flipTests =
+    describe "flip"
+        [ fuzz bool "negates the given boolean value" <|
+            \value ->
+                flip value |> Expect.equal (not value)
+        ]
+
+
+multiplyFloat : Float -> Int -> Float
+multiplyFloat x y =
+    x * toFloat y
+
+
+multiplyFloatTests : Test
+multiplyFloatTests =
+    describe "multiplyFloat"
+        [ fuzz2 float int "multiplies given numbers" <|
+            \x y ->
+                multiplyFloat x y
+                    |> Expect.within (Expect.Absolute 0.0000001) (x * toFloat y)
+        ]
+
+
+pizzaLeft : Float -> Float -> Float
+pizzaLeft eatenPercent totalSlices =
+    totalSlices - (eatenPercent * totalSlices)
+
+
+pizzaLeftTests : Test
+pizzaLeftTests =
+    describe "pizzaLeft"
+        [ fuzz2 percentage float "returns remaining pizza slices" <|
+            \eaten total ->
+                pizzaLeft eaten total
+                    |> Expect.within (Expect.Absolute 0.000000001) (total - (eaten * total))
+        ]
+
+
+stringTests : Test
+stringTests =
+    describe "The String module"
+        [ describe "String.reverse"
+            [ test "has no effect on a palindrome" <|
+                -- Unit Test - 1
+                \_ ->
+                    let
+                        palindrome =
+                            "hannah"
+                    in
+                    palindrome
+                        |> String.reverse
+                        |> Expect.equal palindrome
+            , test "reverses a known string" <|
+                -- Unit Test - 2
+                \_ ->
+                    "ABCDEFG"
+                        |> String.reverse
+                        |> Expect.equal "GFEDCBA"
+            , fuzz string "restores the original string if you run it again" <|
+                -- Fuzz Test
+                \randomlyGeneratedString ->
+                    randomlyGeneratedString
+                        |> String.reverse
+                        |> String.reverse
+                        |> Expect.equal randomlyGeneratedString
+            ]
+        ]
+
+
+listLengthTests : Test
+listLengthTests =
+    describe "List.length"
+        [ fuzz (list int) "never returns a negative value" <|
+            \intList ->
+                intList
+                    |> List.length
+                    |> Expect.atLeast 0
+        ]
+
+
+arrayGetTests : Test
+arrayGetTests =
+    describe "Array.get"
+        [ fuzz (array (intRange -20 20)) "returns Nothing for out of range index" <|
+            \intArray ->
+                let
+                    length =
+                        Array.length intArray
+                in
+                intArray
+                    |> Array.get length
+                    |> Expect.equal Nothing
+        , fuzz (tuple ( int, int )) "adds two given integers (tuple)" <|
+            \( num1, num2 ) ->
+                add num1 num2
+                    |> Expect.equal (num1 + num2)
+        , fuzz2 int int "adds two given integers (fuzz2)" <|
+            \num1 num2 ->
+                add num1 num2 |> Expect.equal (num1 + num2)
+        ]
+
+
+listReverseTests : Test
+listReverseTests =
+    describe "List.reverse"
+        [ fuzz (tuple ( list int, int )) "doesn't remove a member of the list" <|
+            \( intList, num ) ->
+                intList
+                    |> List.reverse
+                    |> List.member num
+                    |> Expect.equal (List.member num intList)
+        , fuzz2 (list int) int "doesn't remove a member of the list (fuzz2)" <|
+            \intList num ->
+                intList
+                    |> List.reverse
+                    |> List.member num
+                    |> Expect.equal (List.member num intList)
+        ]
