335baa1de91316b3e350b4f62aff5dd43e8160b4
Derek Wan <d.wan@icloud.com>
2021-01-19 09:58:44 +0800

    Flesh out more of the POST

--- beginning-elm/post-app/Page/NewPost.elm
+++ beginning-elm/post-app/Page/NewPost.elm
@@ -7,7 +7,8 @@ import Error exposing (buildErrorMessage)
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (onClick, onInput)
-import Post exposing (Post, PostId)
+import Http
+import Post exposing (Post, PostId, postDecoder, postEncoder)
 import Route
 
 
@@ -23,6 +24,8 @@ type Msg
     = StoreTitle String
     | StoreAuthorName String
     | StoreAuthorUrl String
+    | CreatePost
+    | PostCreated (Result Http.Error Post)
 
 
 
@@ -51,5 +54,38 @@ view model =
 newPostForm : Html Msg
 newPostForm =
     Html.form []
-        [ div [] [ text "Title", br [] [], input [ type_ "text", onInput StoreTitle ] [] ]
+        [ div []
+            [ text "Title"
+            , br [] []
+            , input [ type_ "text", onInput StoreTitle ] []
+            ]
+        , br [] []
+        , div []
+            [ text "Author Name"
+            , br [] []
+            , input [ type_ "text", onInput StoreAuthorName ] []
+            ]
+        , br [] []
+        , div []
+            [ text "Author URL"
+            , br [] []
+            , input [ type_ "text", onInput StoreAuthorUrl ] []
+            ]
+        , br [] []
+        , div []
+            [ button [ type_ "button", onClick CreatePost ] [ text "Submit" ]
+            ]
         ]
+
+
+
+-- update
+
+
+createPost : Post -> Cmd Msg
+createPost post =
+    Http.post
+        { url = "http://localhost:5019/posts"
+        , body = Http.jsonBody (postEncoder post)
+        , expect = Http.expectJson PostCreated postDecoder
+        }
