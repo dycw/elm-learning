4fecd975201071785687b68c92e041d4b8259e59
Derek Wan <d.wan@icloud.com>
2021-01-20 15:50:26 +0800

    Apply List.map

--- elm-in-action/src/PhotoGroove.elm
+++ elm-in-action/src/PhotoGroove.elm
@@ -1,12 +1,17 @@
 module PhotoGroove exposing (main)
 
+import Array
 import Html exposing (Html, div, h1, img, text)
 import Html.Attributes exposing (class, id, src)
 import String exposing (String)
 
 
 type alias Model =
-    List { url : String }
+    List Thumbnail
+
+
+type alias Thumbnail =
+    { url : String }
 
 
 type Msg
@@ -31,11 +36,19 @@ view model =
     div [ class "content" ]
         [ h1 [] [ text "Photo Groove" ]
         , div [ id "thumbnails" ]
-            [ img [ src "http://elm-in-action.com/1.jpeg" ] []
-            , img [ src "http://elm-in-action.com/2.jpeg" ] []
-            , img [ src "http://elm-in-action.com/3.jpeg" ] []
-            ]
+            (List.map viewThumbnail initModel)
         ]
+
+
+viewThumbnail : Thumbnail -> Html msg
+viewThumbnail thumb =
+    img [ src (urlPrefix ++ thumb.url) ] []
+
+
+
+-- img [ src "http://elm-in-action.com/1.jpeg" ] []
+-- , img [ src "http://elm-in-action.com/2.jpeg" ] []
+-- , img [ src "http://elm-in-action.com/3.jpeg" ] []
 
 
 main : Html Msg
