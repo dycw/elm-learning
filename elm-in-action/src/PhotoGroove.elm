d3e67ad9ab99e6f361459409737cf89d16ed54a0
Derek Wan <d.wan@icloud.com>
2021-01-20 16:14:39 +0800

    Use partial application

--- elm-in-action/src/PhotoGroove.elm
+++ elm-in-action/src/PhotoGroove.elm
@@ -41,7 +41,7 @@ view model =
     div [ class "content" ]
         [ h1 [] [ text "Photo Groove" ]
         , div [ id "thumbnails" ]
-            (List.map (\t -> viewThumbnail model.selectedUrl t) model.photos)
+            (List.map (viewThumbnail model.selectedUrl) model.photos)
         , img
             [ class "large"
             , src (urlPrefix ++ "large/" ++ model.selectedUrl)
