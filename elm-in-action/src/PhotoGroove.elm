c142031676e423d25a72e0eaf006ec0dc903cc43
Derek Wan <d.wan@icloud.com>
2021-01-20 16:49:04 +0800

    Start elm-in-action (#7)
    * WIP protoype initial
    
    * Starting elm-in-action
    
    * Add annotations
    
    * Add index.html
    
    * Add initModel
    
    * Add URL prefix
    
    * Apply List.map
    
    * Have larger photo available
    
    * Refactoring in viewThumbnail
    
    * Use partial application
    
    * More refactoring
    
    * Remove dead code
    
    * Apply some destructuring
    
    * Remove unused import
    
    * Write an update function
    
    * Change viewThumbnail
    
    * Git ignore app.js and elm.js
    
    * Git ignore index.html too
    
    * Now use Browser.sandbox
    
    * Bump version: 0.0.5 → 0.0.6

---
+++ elm-in-action/src/PhotoGroove.elm
@@ -0,0 +1,74 @@
+module PhotoGroove exposing (main)
+
+import Browser
+import Html exposing (Html, div, h1, img, text)
+import Html.Attributes exposing (class, classList, id, src)
+import Html.Events exposing (onClick)
+import String exposing (String)
+
+
+type alias Model =
+    { photos : List Picture
+    , selectedUrl : String
+    }
+
+
+type alias Picture =
+    { url : String }
+
+
+type alias Msg =
+    { description : String, data : String }
+
+
+initModel : Model
+initModel =
+    { photos =
+        [ { url = "1.jpeg" }
+        , { url = "2.jpeg" }
+        , { url = "3.jpeg" }
+        ]
+    , selectedUrl = "1.jpeg"
+    }
+
+
+urlPrefix : String
+urlPrefix =
+    "http://elm-in-action.com/"
+
+
+view : Model -> Html Msg
+view { photos, selectedUrl } =
+    div [ class "content" ]
+        [ h1 [] [ text "Photo Groove" ]
+        , div [ id "thumbnails" ]
+            (List.map (viewThumbnail selectedUrl) photos)
+        , img
+            [ class "large", src (urlPrefix ++ "large/" ++ selectedUrl) ]
+            []
+        ]
+
+
+viewThumbnail : String -> Picture -> Html Msg
+viewThumbnail selectedUrl { url } =
+    img
+        [ src (urlPrefix ++ url)
+        , classList [ ( "selected", selectedUrl == url ) ]
+        , onClick { description = "ClickedPhoto", data = url }
+        ]
+        []
+
+
+update : Msg -> Model -> Model
+update { description, data } model =
+    if description == "ClickedPhoto" then
+        { model | selectedUrl = data }
+
+    else
+        model
+
+
+main : Program () Model Msg
+main =
+    Browser.sandbox
+        { init = initModel, update = update, view = view }
