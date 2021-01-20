0fd212e5395f2ec6b9500cbedf5e8cbb7c3eba2d
Derek Wan <d.wan@icloud.com>
2021-01-21 00:12:44 +0800

    Browser.element can show random images now (#8)
    * Reorder init/view/update
    
    * Add a button
    
    * Use a case expression
    
    * Correct photoListUrl
    
    * Re-add index.html
    
    * Use style sheet
    
    * Add 'make' executable
    
    * Add ThumbnailSize
    
    * Add two view utilities
    
    * Add the radio buttons
    
    * Add 'class' to the div
    
    * Add getPhotoUrl
    
    * Make Msg a union type now
    
    * viewThumbnail returns ClickedPhoto url
    
    * Add randomPhotoPicker
    
    * Add GotSelectedIndex, use Browser.element
    
    * Bump version: 0.0.6 → 0.0.7

--- elm-in-action/src/PhotoGroove.elm
+++ elm-in-action/src/PhotoGroove.elm
@@ -1,74 +1,141 @@
 module PhotoGroove exposing (main)
 
+import Array exposing (Array)
 import Browser
-import Html exposing (Html, div, h1, img, text)
-import Html.Attributes exposing (class, classList, id, src)
+import Html exposing (Html, button, div, h1, h3, img, input, label, text)
+import Html.Attributes exposing (class, classList, id, name, src, type_)
 import Html.Events exposing (onClick)
-import String exposing (String)
+import Random
 
 
 type alias Model =
-    { photos : List Picture
+    { photos : List Photo
     , selectedUrl : String
+    , chosenSize : ThumbnailSize
     }
 
 
-type alias Picture =
+type Msg
+    = ClickedPhoto String
+    | ClickedSize ThumbnailSize
+    | ClickedSurpriseMe
+    | GotSelectedIndex Int
+
+
+type alias Photo =
     { url : String }
 
 
-type alias Msg =
-    { description : String, data : String }
+type ThumbnailSize
+    = Small
+    | Medium
+    | Large
 
 
-initModel : Model
-initModel =
+init : Model
+init =
     { photos =
         [ { url = "1.jpeg" }
         , { url = "2.jpeg" }
         , { url = "3.jpeg" }
         ]
     , selectedUrl = "1.jpeg"
+    , chosenSize = Small
     }
 
 
-urlPrefix : String
-urlPrefix =
+photoArray : Array Photo
+photoArray =
+    Array.fromList init.photos
+
+
+photoListUrl : String
+photoListUrl =
     "http://elm-in-action.com/"
 
 
 view : Model -> Html Msg
-view { photos, selectedUrl } =
+view { photos, selectedUrl, chosenSize } =
     div [ class "content" ]
         [ h1 [] [ text "Photo Groove" ]
-        , div [ id "thumbnails" ]
+        , button [ onClick ClickedSurpriseMe ] [ text "Surprise Me!" ]
+        , h3 [] [ text "Thumbnail size:" ]
+        , div [ id "choose-size" ] (List.map viewSizeChooser [ Small, Medium, Large ])
+        , div [ id "thumbnails", class (sizeToString chosenSize) ]
             (List.map (viewThumbnail selectedUrl) photos)
         , img
-            [ class "large", src (urlPrefix ++ "large/" ++ selectedUrl) ]
+            [ class "large", src (photoListUrl ++ "large/" ++ selectedUrl) ]
             []
         ]
 
 
-viewThumbnail : String -> Picture -> Html Msg
+viewThumbnail : String -> Photo -> Html Msg
 viewThumbnail selectedUrl { url } =
     img
-        [ src (urlPrefix ++ url)
+        [ src (photoListUrl ++ url)
         , classList [ ( "selected", selectedUrl == url ) ]
-        , onClick { description = "ClickedPhoto", data = url }
+        , onClick (ClickedPhoto url)
         ]
         []
 
 
-update : Msg -> Model -> Model
-update { description, data } model =
-    if description == "ClickedPhoto" then
-        { model | selectedUrl = data }
+viewSizeChooser : ThumbnailSize -> Html Msg
+viewSizeChooser size =
+    label []
+        [ input [ type_ "radio", name "size", onClick (ClickedSize size) ] []
+        , text (sizeToString size)
+        ]
 
-    else
-        model
+
+sizeToString : ThumbnailSize -> String
+sizeToString size =
+    case size of
+        Small ->
+            "small"
+
+        Medium ->
+            "med"
+
+        Large ->
+            "large"
+
+
+getPhotoUrl : Int -> String
+getPhotoUrl index =
+    case Array.get index photoArray of
+        Just photo ->
+            photo.url
+
+        Nothing ->
+            ""
+
+
+randomPhotoPicker : Random.Generator Int
+randomPhotoPicker =
+    Random.int 0 (Array.length photoArray - 1)
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        ClickedPhoto url ->
+            ( { model | selectedUrl = url }, Cmd.none )
+
+        ClickedSize size ->
+            ( { model | chosenSize = size }, Cmd.none )
+
+        ClickedSurpriseMe ->
+            ( model, Random.generate GotSelectedIndex randomPhotoPicker )
+
+        GotSelectedIndex index ->
+            ( { model | selectedUrl = getPhotoUrl index }, Cmd.none )
 
 
 main : Program () Model Msg
 main =
-    Browser.sandbox
-        { init = initModel, update = update, view = view }
+    Browser.element
+        { init = \flags -> ( init, Cmd.none )
+        , view = view
+        , update = update
+        , subscriptions = \model -> Sub.none
+        }
