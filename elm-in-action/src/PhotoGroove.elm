ffd7ab70590469256a8cbcf86173bcd3fdad13e0
Derek Wan <d.wan@icloud.com>
2021-01-21 00:28:23 +0800

    Match start of chapter

--- elm-in-action/src/PhotoGroove.elm
+++ elm-in-action/src/PhotoGroove.elm
@@ -8,50 +8,16 @@ import Html.Events exposing (onClick)
 import Random
 
 
-type alias Model =
-    { photos : List Photo
-    , selectedUrl : String
-    , chosenSize : ThumbnailSize
-    }
+urlPrefix : String
+urlPrefix =
+    "http://elm-in-action.com/"
 
 
 type Msg
     = ClickedPhoto String
+    | GotSelectedIndex Int
     | ClickedSize ThumbnailSize
     | ClickedSurpriseMe
-    | GotSelectedIndex Int
-
-
-type alias Photo =
-    { url : String }
-
-
-type ThumbnailSize
-    = Small
-    | Medium
-    | Large
-
-
-init : Model
-init =
-    { photos =
-        [ { url = "1.jpeg" }
-        , { url = "2.jpeg" }
-        , { url = "3.jpeg" }
-        ]
-    , selectedUrl = "1.jpeg"
-    , chosenSize = Small
-    }
-
-
-photoArray : Array Photo
-photoArray =
-    Array.fromList init.photos
-
-
-photoListUrl : String
-photoListUrl =
-    "http://elm-in-action.com/"
 
 
 view : Model -> Html Msg
@@ -64,7 +30,7 @@ view { photos, selectedUrl, chosenSize } =
         , div [ id "thumbnails", class (sizeToString chosenSize) ]
             (List.map (viewThumbnail selectedUrl) photos)
         , img
-            [ class "large", src (photoListUrl ++ "large/" ++ selectedUrl) ]
+            [ class "large", src (urlPrefix ++ "large/" ++ selectedUrl) ]
             []
         ]
 
@@ -72,7 +38,7 @@ view { photos, selectedUrl, chosenSize } =
 viewThumbnail : String -> Photo -> Html Msg
 viewThumbnail selectedUrl { url } =
     img
-        [ src (photoListUrl ++ url)
+        [ src (urlPrefix ++ url)
         , classList [ ( "selected", selectedUrl == url ) ]
         , onClick (ClickedPhoto url)
         ]
@@ -98,6 +64,40 @@ sizeToString size =
 
         Large ->
             "large"
+
+
+type ThumbnailSize
+    = Small
+    | Medium
+    | Large
+
+
+type alias Photo =
+    { url : String }
+
+
+type alias Model =
+    { photos : List Photo
+    , selectedUrl : String
+    , chosenSize : ThumbnailSize
+    }
+
+
+initialModel : Model
+initialModel =
+    { photos =
+        [ { url = "1.jpeg" }
+        , { url = "2.jpeg" }
+        , { url = "3.jpeg" }
+        ]
+    , selectedUrl = "1.jpeg"
+    , chosenSize = Medium
+    }
+
+
+photoArray : Array Photo
+photoArray =
+    Array.fromList initialModel.photos
 
 
 getPhotoUrl : Int -> String
@@ -134,7 +134,7 @@ update msg model =
 main : Program () Model Msg
 main =
     Browser.element
-        { init = \flags -> ( init, Cmd.none )
+        { init = \flags -> ( initialModel, Cmd.none )
         , view = view
         , update = update
         , subscriptions = \model -> Sub.none
