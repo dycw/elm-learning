444c961986fd730cc4bb04b2551c95c43692a1a7
Derek Wan <d.wan@icloud.com>
2021-01-21 18:36:56 +0800

    Make the filter values Floats

--- elm-in-action/src/PhotoGroove.elm
+++ elm-in-action/src/PhotoGroove.elm
@@ -124,7 +124,7 @@ port setFilters : FilterOptions -> Cmd msg
 
 type alias FilterOptions =
     { url : String
-    , filters : List { name : String, amount : Int }
+    , filters : List { name : String, amount : Float }
     }
 
 
@@ -216,9 +216,9 @@ applyFilters model =
                     urlPrefix ++ "large/" ++ selectedUrl
 
                 filters =
-                    [ { name = "Hue", amount = model.hue }
-                    , { name = "Ripple", amount = model.ripple }
-                    , { name = "Noise", amount = model.noise }
+                    [ { name = "Hue", amount = toFloat model.hue / 11 }
+                    , { name = "Ripple", amount = toFloat model.ripple / 11 }
+                    , { name = "Noise", amount = toFloat model.noise / 11 }
                     ]
             in
             ( model, setFilters { url = url, filters = filters } )
