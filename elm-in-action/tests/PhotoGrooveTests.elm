f662be1b5ab8ac500eab7281fcbf9bbcaaa181b3
Derek Wan <d.wan@icloud.com>
2021-01-23 17:58:46 +0800

    Add decoderTest2

--- elm-in-action/tests/PhotoGrooveTests.elm
+++ elm-in-action/tests/PhotoGrooveTests.elm
@@ -1,8 +1,8 @@
-module PhotoGrooveTests exposing (decoderTest)
+module PhotoGrooveTests exposing (decoderTest, decoderTest2)
 
 import Expect exposing (Expectation)
 import Fuzz exposing (Fuzzer, int, list, string)
-import Json.Decode as Decode
+import Json.Decode as Decode exposing (decodeValue)
 import Json.Encode as Encode
 import PhotoGroove exposing (photoDecoder)
 import Test exposing (Test, test)
@@ -14,5 +14,18 @@ decoderTest =
         \_ ->
             """{"url": "fruits.com", "size": 5}"""
                 |> Decode.decodeString photoDecoder
+                |> Result.map .title
+                |> Expect.equal (Ok "(untitled)")
+
+
+decoderTest2 : Test
+decoderTest2 =
+    test "title defaults; v2" <|
+        \_ ->
+            [ ( "url", Encode.string "fruits.com " )
+            , ( "size", Encode.int 5 )
+            ]
+                |> Encode.object
+                |> decodeValue PhotoGroove.photoDecoder
                 |> Result.map .title
                 |> Expect.equal (Ok "(untitled)")
