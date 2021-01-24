0a3ff1fa7ae0a762ef6587d58b7fa146cc70f03a
Derek Wan <d.wan@icloud.com>
2021-01-24 18:35:45 +0800

    Add viewSelectedPhoto etc

--- elm-in-action/src/PhotoFolders.elm
+++ elm-in-action/src/PhotoFolders.elm
@@ -1,7 +1,7 @@
 module PhotoFolders exposing (main)
 
 import Browser
-import Html exposing (..)
+import Html exposing (Html, div, h1, h2, h3, img, span, text)
 import Html.Attributes exposing (class, src)
 import Html.Events exposing (onClick)
 import Http
@@ -65,3 +65,38 @@ main =
         , update = update
         , subscriptions = \_ -> Sub.none
         }
+
+
+type alias Photo =
+    { title : String
+    , size : Int
+    , relatedUrls : List String
+    , url : String
+    }
+
+
+viewSelectedPhoto : Photo -> Html Msg
+viewSelectedPhoto photo =
+    div
+        [ class "selected-photo" ]
+        [ h2 [] [ text photo.title ]
+        , img [ src (urlPrefix ++ "photos/" ++ photo.url ++ "/full") ] []
+        , span [] [ text (String.fromInt photo.size ++ "KB") ]
+        , h3 [] [ text "Related" ]
+        , div [ class "related-photos" ] (List.map viewRelatedPhoto photo.relatedUrls)
+        ]
+
+
+viewRelatedPhoto : String -> Html Msg
+viewRelatedPhoto url =
+    img
+        [ class "related-photo"
+        , onClick (ClickedPhoto url)
+        , src (urlPrefix ++ "photos/" ++ url ++ "/thumb")
+        ]
+        []
+
+
+urlPrefix : String
+urlPrefix =
+    "http://elm-in-action.com/"
