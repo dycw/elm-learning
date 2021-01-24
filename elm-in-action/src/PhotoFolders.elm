ae8b005af1eca3f4093e1dbff31f4f554e0e7b7c
Derek Wan <d.wan@icloud.com>
2021-01-24 18:44:36 +0800

    View now produces a content div

--- elm-in-action/src/PhotoFolders.elm
+++ elm-in-action/src/PhotoFolders.elm
@@ -58,7 +58,21 @@ update msg model =
 
 view : Model -> Html Msg
 view model =
-    h1 [] [ text "The Grooviest Folders the world has ever seen" ]
+    let
+        photoByUrl : String -> Maybe Photo
+        photoByUrl url =
+            Dict.get url model.photos
+
+        selectedPhoto : Html Msg
+        selectedPhoto =
+            case Maybe.andThen photoByUrl model.selectedPhotoUrl of
+                Just photo ->
+                    viewSelectedPhoto photo
+
+                Nothing ->
+                    text ""
+    in
+    div [ class "content" ] [ div [ class "selected-photo" ] [ selectedPhoto ] ]
 
 
 main : Program () Model Msg
