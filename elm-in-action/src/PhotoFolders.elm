c36db6f42d2e7781335b14a52ae10d0b305d23d8
Derek Wan <d.wan@icloud.com>
2021-01-27 22:58:04 +0800

    Finish

--- elm-in-action/src/PhotoFolders.elm
+++ elm-in-action/src/PhotoFolders.elm
@@ -2,8 +2,8 @@ module PhotoFolders exposing (Model, Msg, init, update, view)
 
 import Browser
 import Dict exposing (Dict)
-import Html exposing (Html, div, h1, h2, h3, img, label, span, sub, text)
-import Html.Attributes exposing (class, src)
+import Html exposing (Html, a, div, h1, h2, h3, img, label, span, sub, text)
+import Html.Attributes exposing (class, href, src)
 import Html.Events exposing (onClick)
 import Http
 import Json.Decode as Decode exposing (Decoder, int, list, string)
@@ -63,7 +63,7 @@ update msg model =
             ( { model | selectedPhotoUrl = Just url }, Cmd.none )
 
         GotInitialModel (Ok newModel) ->
-            ( newModel, Cmd.none )
+            ( { newModel | selectedPhotoUrl = model.selectedPhotoUrl }, Cmd.none )
 
         GotInitialModel (Err _) ->
             ( model, Cmd.none )
@@ -107,8 +107,9 @@ type alias Photo =
 
 viewPhoto : String -> Html Msg
 viewPhoto url =
-    div
-        [ class "photo"
+    a
+        [ href "/photos/++url"
+        , class "photo"
         , onClick (ClickedPhoto url)
         ]
         [ text url ]
