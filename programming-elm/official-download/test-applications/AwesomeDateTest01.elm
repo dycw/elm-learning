63d5e2357192c424d5dab7971c6cb06853fa6ab4
Derek Wan <d.wan@icloud.com>
2021-01-28 21:01:39 +0800

    Add official-download

---
+++ programming-elm/official-download/test-applications/AwesomeDateTest01.elm
@@ -0,0 +1,24 @@
+-- START:module
+
+
+module AwesomeDateTest exposing (suite)
+
+-- END:module
+-- START:imports
+
+import Expect
+import Test exposing (..)
+
+
+
+-- END:imports
+-- START:suite
+
+
+suite : Test
+suite =
+    describe "AwesomeDate" []
+
+
+
+-- END:suite
