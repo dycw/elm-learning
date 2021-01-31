b0f331ad322062a5be0fce0418d88a3e7978f770
Derek Wan <d.wan@icloud.com>
2021-01-31 20:43:31 +0800

    Add 4th test

--- programming-elm/awesome-date/tests/AwesomeDateTest.elm
+++ programming-elm/awesome-date/tests/AwesomeDateTest.elm
@@ -33,4 +33,16 @@ testIsLeapYear =
             \_ ->
                 Date.isLeapYear 2012
                     |> Expect.true "Expected leap year"
+        , test "returns false if not divisible by 4" <|
+            \_ ->
+                Date.isLeapYear 2010
+                    |> Expect.false "Did not expect leap year"
+        , test "returns false if divisible by 4 and 100 but not 400" <|
+            \_ ->
+                Date.isLeapYear 3000
+                    |> Expect.false "Did not expect leap year"
+        , test "returns true if divisible by 4, 100, and 400" <|
+            \_ ->
+                Date.isLeapYear 2000
+                    |> Expect.true "Expected leap year"
         ]
