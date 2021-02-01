9ed8774f4b3bee2d3901f5eef184dec32a94a63f
Derek Wan <d.wan@icloud.com>
2021-02-01 22:29:46 +0800

    Start working on SPA

--- programming-elm/picshare-spa/src/Main.elm
+++ programming-elm/picshare-spa/src/Main.elm
@@ -1,36 +1,69 @@
 module Main exposing (main)
 
-import Browser
+import Browser exposing (Document, UrlRequest)
+import Browser.Navigation as Navigation
 import Html exposing (Html, a, div, h1, i, text)
 import Html.Attributes exposing (class)
+import Routes
+import Url exposing (Url)
 
 
 
 ---- MODEL ----
 
 
+type Page
+    = PublicFeed
+    | Account
+    | NotFound
+
+
 type alias Model =
-    {}
+    { page : Page, navigationKey : Navigation.Key }
 
 
-initialModel : Model
-initialModel =
-    {}
+initialModel : Navigation.Key -> Model
+initialModel navigationKey =
+    { page = NotFound, navigationKey = navigationKey }
 
 
-init : () -> ( Model, Cmd Msg )
-init () =
-    ( initialModel, Cmd.none )
+init : () -> Url -> Navigation.Key -> ( Model, Cmd Msg )
+init () url navigationKey =
+    setNewPage (Routes.match url) (initialModel navigationKey)
 
 
 
 ---- VIEW ----
 
 
-view : Model -> Html Msg
+view : Model -> Document Msg
 view model =
-    div []
-        [ h1 [] [ text "Single Page Applications" ] ]
+    let
+        ( title, content ) =
+            viewContent model.page
+    in
+    { title = title
+    , body = [ content ]
+    }
+
+
+viewContent : Page -> ( String, Html Msg )
+viewContent page =
+    case page of
+        PublicFeed ->
+            ( "Picshare"
+            , h1 [] [ text "Public Feed" ]
+            )
+
+        Account ->
+            ( "Account"
+            , h1 [] [ text "Account" ]
+            )
+
+        NotFound ->
+            ( "Not Found"
+            , div [ class "not-found" ] [ h1 [] [ text "Page Not Found" ] ]
+            )
 
 
 
@@ -38,12 +71,31 @@ view model =
 
 
 type Msg
-    = NoOp
+    = NewRoute (Maybe Routes.Route)
+    | Visit UrlRequest
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-    ( model, Cmd.none )
+    case msg of
+        NewRoute maybeRoute ->
+            setNewPage maybeRoute model
+
+        Visit urlRequest ->
+            ( model, Cmd.none )
+
+
+setNewPage : Maybe Routes.Route -> Model -> ( Model, Cmd Msg )
+setNewPage maybeRoute model =
+    case maybeRoute of
+        Just Routes.Home ->
+            ( { model | page = PublicFeed }, Cmd.none )
+
+        Just Routes.Account ->
+            ( { model | page = Account }, Cmd.none )
+
+        Nothing ->
+            ( { model | page = NotFound }, Cmd.none )
 
 
 subscriptions : Model -> Sub Msg
@@ -57,9 +109,11 @@ subscriptions model =
 
 main : Program () Model Msg
 main =
-    Browser.element
+    Browser.application
         { init = init
         , view = view
         , update = update
         , subscriptions = subscriptions
+        , onUrlRequest = Visit
+        , onUrlChange = Routes.match >> NewRoute
         }
